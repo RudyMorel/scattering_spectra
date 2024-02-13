858550d1ddea4a37b3905297b8263ab0282a2364
RudyMorel <rudy.morel@ens.fr>
2024-02-13 12:43:32 -0500

    - adding complex AvgPool1d as estimator

--- scatspectra/layers/statistics.py
+++ scatspectra/layers/statistics.py
@@ -40,6 +40,40 @@ class WindowSelector(Estimator):
 
     def forward(self, x: torch.Tensor, **kwargs) -> torch.Tensor:
         return x[..., self.w]
+    
+
+class AvgPooling(Estimator):
+    """ Average pooling operator for complex dtype tensor. """
+
+    def __init__(self, kernel_size, stride):
+        super(AvgPooling, self).__init__()
+        self.pool = torch.nn.AvgPool1d(kernel_size, stride)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ 
+        Average pooling operator for complex dtypes.
+
+        :param x: any dimension tensor.
+        :return:
+        """
+        if x.is_complex():
+
+            # Separate real and imaginary parts.
+            x_real = x.real
+            x_imag = x.imag
+
+            # Perform average pooling on real and imaginary parts separately.
+            x_real = self.pool(x_real.view(x.shape[0], -1, x.shape[-1]))
+            x_imag = self.pool(x_imag.view(x.shape[0], -1, x.shape[-1]))
+
+            # Combine real and imaginary parts back into a complex tensor.
+            y = torch.view_as_complex(torch.stack([x_real, x_imag], dim=-1))
+
+        else:
+
+            y = self.pool(x.view(x.shape[0], -1, x.shape[-1]))
+
+        return y.view(x.shape[:-1] + (-1, ))
 
 
 class AverageLowPass(nn.Module):
