7dbc00a44feb47757a84db35d9f188ab3e0368f8
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-02-27 22:55:48 +0100

    Refactored file

--- scatspectra/data/__init__.py
+++ scatspectra/data/__init__.py
@@ -1,12 +1,22 @@
-import pickle
+import pandas as pd
 import pkg_resources
 
-# To load the data from a piclke file
-def load_data(filename):
-    filepath = pkg_resources.resource_filename(__name__, 'data/' + filename)
-    with open(filepath, 'rb') as file:
-        data = pickle.load(file)
-    return data
 
-# Variable to access the data
-snp_data = load_data('snp_WSJ_08_02_2024.pkl')
+def load_data(filename: str, /) -> pd.DataFrame:
+    """Loads a dataset from the package data folder.
+
+    Args:
+        filename (str): file name
+
+    Returns:
+        pd.DataFrame: dataset
+    """
+    filepath = pkg_resources.resource_filename(__name__, "data/" + filename)
+    if filepath.endswith(".csv"):
+        return pd.read_csv(filepath)
+    if filepath.endswith(".json"):
+        return pd.read_json(filepath)
+    return pd.read_pickle(filepath)
+
+
+snp_data = load_data("snp_WSJ_08_02_2024.pkl")  # SNP data
