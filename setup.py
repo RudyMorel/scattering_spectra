080bc051ad03577100586a8967f645c26e9c326e
sarahnourgh <ghaithsarahnour@gmail.com>
2024-02-27 21:53:33 +0100

    updating setup file

--- setup.py
+++ setup.py
@@ -19,4 +19,4 @@ setuptools.setup(
     license='MIT',
     install_requires=reqs,
     packages=setuptools.find_packages(),
-    include_package_data=True)
+    package_data={'scatspectra': ['data/snp_WSJ_08_02_2024.csv']})
